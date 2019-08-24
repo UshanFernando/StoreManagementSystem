84a211ba2cb29cfa04e62495826683374f233e76
HarithWijayawardhana <harytw456@gmail.com>
2019-08-25 02:41:59 +0530

    version 0.0.1 base *revert the Errors and

--- src/model/Requests.java
+++ src/model/Requests.java
@@ -1,4 +1,50 @@
 package model;
 
+import java.util.Date;
+
 public class Requests {
+    private int id;
+    private String status;
+    private double amount;
+    private Date date;
+
+    public Requests(int id, String status, double amount, Date date) {
+
+        this.id = id;
+        this.status = status;
+        this.amount = amount;
+        this.date = date;
+
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAmount() {
+        return status;
+    }
+
+    public void setAmount(String status) {
+        this.status = status;
+    }
+
+    public boolean isActive(){
+
+        return status.equals("Available");
+    }
+
 }

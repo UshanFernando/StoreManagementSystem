30cb59cc73d1dfbda22b589badd24678b81f457e
vidura21 <vidurasamarasiri@gmail.com>
2019-09-23 20:42:53 +0530

    version 1.0.0.1 base
    * added attendance and leave management

---
+++ src/service/LeaveManagerServiceInterface.java
@@ -0,0 +1,23 @@
+package service;
+
+
+import javafx.collections.ObservableList;
+import model.Leave;
+import util.Constants;
+import util.DBConnection;
+import util.QueryUtil;
+
+
+
+
+public interface LeaveManagerServiceInterface {
+
+
+    public boolean addLeave(Leave user);
+
+
+
+
+
+
+}
